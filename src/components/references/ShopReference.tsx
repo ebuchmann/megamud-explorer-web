2a3ac213a7da854cb1ac7d1a0cd6f320ce1ea379
Eric Buchmann <me@ebuchmann.com>
2024-05-11 06:42:40 -0700

    add tooltip for shops

--- src/components/references/ShopReference.tsx
+++ src/components/references/ShopReference.tsx
@@ -1,25 +1,29 @@
 import { A } from '@solidjs/router';
 import { shopData } from '../../data/shops';
-import { createEffect, createSignal } from 'solid-js';
-import { Shop } from '../../types';
+import { Tooltip } from '../Tooltip';
 
 type ShopReferenceProps = {
   number: number;
 };
 
-export function ShopReference(props: ShopReferenceProps) {
-  const [shop, setShop] = createSignal<Shop>();
-
-  createEffect(() => {
-    setShop(shopData.find((sh) => sh.Number === props.number));
-  });
+export function ShopReference({ number }: ShopReferenceProps) {
+  const shop = shopData.find((sh) => sh.Number === number);
 
   return (
-    <A
-      class="decoration-dotted underline underline-offset-2 hover:text-sky-300"
-      href={`/shops/${props.number}`}
+    <Tooltip
+      placement="left"
+      content={
+        <div class="flex flex-col">
+          <div>Markup: {shop?.['Markup%']}%</div>
+        </div>
+      }
     >
-      {shop()?.Name}
-    </A>
+      <A
+        class="decoration-dotted underline underline-offset-2 hover:text-sky-300"
+        href={`/shops/${number}`}
+      >
+        {shop?.Name}
+      </A>
+    </Tooltip>
   );
 }
